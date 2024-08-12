a2aaa37d1da5851dd2efae774d0f2ad356d2f4b9
RobWPink <101643219+RobWPink@users.noreply.github.com>
2024-08-12 16:29:01 -0400

    added --file option

--- modbusPi.py
+++ modbusPi.py
@@ -82,89 +82,135 @@ parser = optparse.OptionParser(usage=usage)
 parser.add_option('-r','--read',action="store",type="string",dest='read',help ='Specify settings of device to read from.')
 parser.add_option('-w','--write',action="store",type="string",dest='write',help ='Specify settings of device to write to.')
 parser.add_option('-p','--pipe',action="store_true",dest ='pipe',help='Pipe output from read to input for write. Write no longer takes data, just offset')
+parser.add_option('-f','--file',action="store",type="string",dest ='fileOp',help='Take read data and write it to given file')
 (options, args) = parser.parse_args()
 if options.pipe:
 	if not options.read or not options.write:
 		parser.error('Pipe (-p) option requires BOTH read and write options to be inputted')
+if options.fileOp:
+	if options.read and options.write:
+		parser.error('Please only use file option to read to file OR write from file')
+	if not options.read and not options.write:
+		parser.error('file option requires either a read or write command to accompany it')
 
 readList = []
 writeList = []
+trys = 0
+while(1):
+	try:
+		if options.read:
+			readList = checkList(options.read)
+			#if int(readList[5]) > 124:
 
-if options.read:
-	readList = checkList(options.read)
-
-	if len(readList) > 6:
-		del readList[6:len(readList)]
-	readList[5] = int(readList[5])
-
-	if(valid_IP_Address(readList[0])):
-		readTCP = ModbusClient(host=readList[0], port=readList[1], unit_id=1, auto_open=True)
-		if readList[3] == 1:
-			outp = readTCP.read_coils(readList[4],readList[5])
-		elif readList[3] == 2:
-			outp = readTCP.read_discrete_inputs(readList[4],readList[5])
-		elif readList[3] == 4:
-			outp = readTCP.read_input_registers(readList[4],readList[5])
-		elif readList[3] == 3:
-			outp = readTCP.read_holding_registers(readList[4],readList[5])
-	else:
-		readDev = minimalmodbus.Instrument(readList[1], readList[0])  # port name, slave address (in decimal)
-		readDev.serial.baudrate = readList[2]         # Baud
-		readDev.serial.bytesize = 8
-		readDev.serial.parity   = serial.PARITY_NONE
-		readDev.serial.stopbits = 1
-		readDev.serial.timeout  = 0.05          # seconds
-
-		if readList[3] < 3:
-			outp = readDev.read_bits(readList[4],readList[5],readList[3])
-		else:
-			print(readList[5])
-			outp = readDev.read_registers(readList[4],readList[5],readList[3])
-	print("Read: ",outp)
-if options.write:
-	writeList = checkList(options.write)
-
-	if not options.pipe:
-		if len(writeList) > 6:
-			dataList = []
-			for x in range(5,len(writeList)):
-				if any(c.isalpha() for c in writeList[x]):
-					print("Please convert all string or char data to decimal")
-					sys.exit()
+			if len(readList) > 6:
+				del readList[6:len(readList)]
+			readList[5] = int(readList[5])
+    
+			if(valid_IP_Address(readList[0])):
+				readTCP = ModbusClient(host=readList[0], port=readList[1], unit_id=1, auto_open=True)
+				if readList[3] == 1:
+					outp = readTCP.read_coils(readList[4],readList[5])
+				elif readList[3] == 2:
+					outp = readTCP.read_discrete_inputs(readList[4],readList[5])
+				elif readList[3] == 4:
+					outp = readTCP.read_input_registers(readList[4],readList[5])
+				elif readList[3] == 3:
+					outp = readTCP.read_holding_registers(readList[4],readList[5])
+			else:
+				readDev = minimalmodbus.Instrument(readList[1], readList[0])  # port name, slave address (in decimal)
+				readDev.serial.baudrate = readList[2]         # Baud
+				readDev.serial.bytesize = 8
+				readDev.serial.parity   = serial.PARITY_NONE
+				readDev.serial.stopbits = 1
+				readDev.serial.timeout  = 1          # seconds
+		
+				if readList[3] < 3:
+					outp = readDev.read_bits(readList[4],readList[5],readList[3])
 				else:
-					dataList.append(int(writeList[x]))
-					
-			writeList[5] = dataList
-			del writeList[6:len(writeList)]
+					outp = readDev.read_registers(readList[4],readList[5],readList[3])
+		print("Read: ",outp)
+		if not options.write and not options.fileOp:
+			sys.exit()
+    
+		if options.fileOp:
+			if options.read:
+				with open(options.fileOp, 'w+') as fp:
+					for item in outp:
+						fp.write("%s\n" % item)
+				sys.exit()  
+			elif options.write:
+				with open(options.fileOp) as file:
+					for line in file:
+						outp.append(line.rstrip())
+		
+		
+			if options.write:
+				writeList = checkList(options.write)
+			
+				if not options.pipe:
+					if len(writeList) > 6:
+						dataList = []
+						for x in range(5,len(writeList)):
+							if any(c.isalpha() for c in writeList[x]):
+								print("Please convert all string or char data to decimal")
+								sys.exit()
+							else:
+								dataList.append(int(writeList[x]))
+								
+						writeList[5] = dataList
+						del writeList[6:len(writeList)]
+					else:
+						writeList[5] = int(writeList[5])
+				else:
+					del writeList[6:len(writeList)]
+			
+				if(valid_IP_Address(writeList[0])):
+					writeTCP = ModbusClient(host=writeList[0], port=writeList[1], unit_id=2, auto_open=True)
+					if writeList[3] == 1:
+						writeTCP.write_multiple_coils(writeList[4],writeList[5])
+					elif writeList[3] == 3:
+						writeTCP.write_multiple_registers(writeList[4],writeList[5])
+				else:
+					writeDev = minimalmodbus.Instrument(writeList[1], writeList[0])  # port name, slave address (in decimal)
+					writeDev.serial.baudrate = writeList[2]         # Baud
+					writeDev.serial.bytesize = 8
+					writeDev.serial.parity   = serial.PARITY_NONE
+					writeDev.serial.stopbits = 1
+					writeDev.serial.timeout  = 1          # seconds
+			
+					if writeList[3] == 1:
+						if options.pipe or options.fileOp:
+							writeDev.write_bits(writeList[4],outp,15)
+						else:
+							writeDev.write_bits(writeList[4],writeList[5],15)
+			
+					elif writeList[3] == 3:
+						if options.pipe or options.fileOp:
+							writeDev.write_registers(writeList[4],outp)
+						else:
+							writeDev.write_registers(writeList[4],writeList[5])
+					else:
+						print("unable to write to read only data types")
+				sys.exit()
+	except minimalmodbus.NoResponseError:
+		print("No connection found...")
+		if trys > 10:
+			print("exiting")
+			sys.exit()
 		else:
-			writeList[5] = int(writeList[5])
-	else:
-		del writeList[6:len(writeList)]
-
-	if(valid_IP_Address(writeList[0])):
-		writeTCP = ModbusClient(host=writeList[0], port=writeList[1], unit_id=2, auto_open=True)
-		if writeList[3] == 1:
-			outp = writeTCP.write_coils(writeList[4],writeList[5])
-		elif writeList[3] == 3:
-			outp = writeTCP.write_holding_registers(writeList[4],writeList[5])
-	else:
-		writeDev = minimalmodbus.Instrument(writeList[1], writeList[0])  # port name, slave address (in decimal)
-		writeDev.serial.baudrate = writeList[2]         # Baud
-		writeDev.serial.bytesize = 8
-		writeDev.serial.parity   = serial.PARITY_NONE
-		writeDev.serial.stopbits = 1
-		writeDev.serial.timeout  = 0.05          # seconds
-
-		if writeList[3] == 1:
-			if options.pipe:
-				writeDev.write_bits(writeList[4],outp,15)
-			else:
-				writeDev.write_bits(writeList[4],writeList[5],15)
-
-		elif writeList[3] == 3:
-			if options.pipe:
-				writeDev.write_registers(writeList[4],outp)
-			else:
-				writeDev.write_registers(writeList[4],writeList[5])
+			print("retrying")
+			time.sleep(1)
+			trys = trys+1
+	except minimalmodbus.InvalidResponseError:
+		print("incomplete data recieved...")
+		if trys > 10:
+			print("exiting")
+			sys.exit()
 		else:
-			print("unable to write to read only data types")
+			print("retrying")
+			time.sleep(1)
+			trys = trys+1
+	except KeyboardInterrupt:
+		print("manual stop")
+		sys.exit()
+  
